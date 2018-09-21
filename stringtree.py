411855f96deb09fbfbfbac975fd93e6f85d57bca
Marton Gunyho <andras.gunyho@aalto.fi>
2018-09-21 16:17:43 +0300

    Add some classes to stringree, probably useless.

--- stringtree.py
+++ stringtree.py
@@ -1,11 +1,48 @@
-tree = {
+STRING_TREE = {
     "Mitä olet tehnyt tänään?" : {
         "Liikunta" : {
             "Asteikolla 0-5, kuinka intensiivistä se oli?" : {
                 "Kuinka kauan liikunta kesti tunteina?" : {
                     "Hienosti."
-                }   
+                }
             }
         }
     }
 }
+
+class ReplyMessage():
+    def __init__(self):
+        pass
+
+class ReplyMessageWithChoices(ReplyMessage):
+    def __init__(self, children):
+        super(ReplyMessageWithChoices, self).__init__()
+        self._children = children
+
+class StringTree():
+    def __init__(self):
+        self.tree = STRING_TREE
+        self.current_message = list(STRING_TREE.keys())[0]
+        self.message_chain = []
+
+    def get_reply(self, message_str):
+        reply = None
+
+        try:
+            reply = current_message[message_str]
+            self.message_chain.append(self.current_message)
+            self.current_message = reply
+
+        except ValueError:
+            reply = "asd"
+
+        #return list(reply.keys())
+        if type(reply) == str:
+            return reply
+        else:
+            assert type(reply) == dict
+            return list(reply.keys())[0]
+
+    def go_back(self):
+        if self.message_chain:
+            self.current_message = self.message_chain.pop()
