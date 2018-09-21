b82609d87e0e565a41df2299f1b9470317c4b631
Tommi Anttila <tommila@dhcp-87-206.eduroam.aalto.fi>
2018-09-21 15:44:04 +0300

    Stringtree added.

---
+++ stringtree.py
@@ -0,0 +1,11 @@
+tree = {
+    "Mitä olet tehnyt tänään?" : {
+        "Liikunta" : {
+            "Asteikolla 0-5, kuinka intensiivistä se oli?" : {
+                "Kuinka kauan liikunta kesti tunteina?" : {
+                    "Hienosti."
+                }   
+            }
+        }
+    }
+}
