d53143969de4800019ec619a7102f12d4a6005fe
mgunyho <marci.gunyho@gmail.com>
2018-10-06 15:07:31 +0300

    Add params to scoreobj.

--- scoring.py
+++ scoring.py
@@ -10,11 +10,15 @@ BAD_KEY = "bad"
 
 class ScoreObject():
 
-  def __init__(self, value, _type):
+  def __init__(self, value, _type, params):
+    """
+    params: the parameters that were used to calculate value
+    """
     assert _type in [GOOD_KEY, BAD_KEY]
     value = float(value)
     self.type = _type
     self.value = value
+    self.parameters = params
 
   def __repr__(self):
     return "{}({})".format(self.__class__, self.__dict__)
@@ -26,11 +30,11 @@ def liikunta_score(params):
   print("liikunta_score(): {}".format(params))
   score = params[1] * params[2] #TODO: replace this with something that is not linear in duration
 
-  return ScoreObject(score, "good")
+  return ScoreObject(score, "good", params)
 
 def alkoholi_score(params):
   # params: should just be a float
-  return ScoreObject(abs(params), "bad")
+  return ScoreObject(abs(params), "bad", params)
 
 
 ########################
