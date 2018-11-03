b84fb5b5ff62f323bb144c34785f89e3dfe9709b
Marton Gunyho <andras.gunyho@aalto.fi>
2018-11-03 21:31:38 +0200

    Change for-loop to splitapply.

--- analysis.m
+++ analysis.m
@@ -45,47 +45,50 @@ endDate = datetime(datetime(badSorted(2, end), 'ConvertFrom', 'posixtime').Year,
 days = startDate:endDate;
 points = zeros(3, length(days));
 
-for d = 1:length(days)
-    for i  = 1:length(goodSorted)
-        goodDt = datetime(goodSorted(2, i), 'ConvertFrom', 'posixtime');
-        if datetime(goodDt.Year, goodDt.Month, goodDt.Day) == days(d)
-            points(1, d) = points(1, d) + goodSorted(1, i);
-        end
-    end
-    for i  = 1:length(badSorted)
-        badDt = datetime(badSorted(2, i), 'ConvertFrom', 'posixtime');
-        if datetime(badDt.Year, badDt.Month, badDt.Day)== days(d)
-            points(2, d) = points(2, d) + badSorted(1, i);
-        end
-    end
-    for i  = 1:length(all)
-        allDt = datetime(all(2, i), 'ConvertFrom', 'posixtime');
-        if datetime(allDt.Year, allDt.Month, allDt.Day)== days(d)
-            points(3, d) = points(3, d) + all(1, i);
-        end
-    end
-end
+%for d = 1:length(days)
+%    for i  = 1:length(goodSorted)
+%        goodDt = datetime(goodSorted(2, i), 'ConvertFrom', 'posixtime');
+%        if datetime(goodDt.Year, goodDt.Month, goodDt.Day) == days(d)
+%            points(1, d) = points(1, d) + goodSorted(1, i);
+%        end
+%    end
+%    for i  = 1:length(badSorted)
+%        badDt = datetime(badSorted(2, i), 'ConvertFrom', 'posixtime');
+%        if datetime(badDt.Year, badDt.Month, badDt.Day)== days(d)
+%            points(2, d) = points(2, d) + badSorted(1, i);
+%        end
+%    end
+%    for i  = 1:length(all)
+%        allDt = datetime(all(2, i), 'ConvertFrom', 'posixtime');
+%        if datetime(allDt.Year, allDt.Month, allDt.Day)== days(d)
+%            points(3, d) = points(3, d) + all(1, i);
+%        end
+%    end
+%end
+
 
 goodDate = datetime(goodSorted(2, :), 'ConvertFrom', 'posixtime' );
 badDate = datetime(badSorted(2, :), 'ConvertFrom', 'posixtime' );
 allDate = datetime(all(2, :), 'ConvertFrom', 'posixtime' );
 
+datesGoodShifted = dateshift(goodDate, 'start', 'day');
+datesBadShifted = dateshift(badDate, 'start', 'day');
+
+goodSums = splitapply(@sum, goodSorted(1,:), findgroups(datesGoodShifted));
+badSums = splitapply(@sum, badSorted(1,:), findgroups(datesBadShifted));
+
+%error('asd');
+
 goodDate.Format = 'dd-MMMM-yyyy';
 badDate.Format = 'dd-MMMM-yyyy';
 allDate.Format = 'dd-MMMM-yyyy';
-
-
 
 figure 
 hold on
 %grid on
 datetick('x','dd-mm-yyyy','keeplimits','keepticks')
-plot(days, cumsum(points(1, :)))
-plot(days, cumsum(points(2, :)))
-plot(days, cumsum(points(3, :)))
+plot(days, cumsum(goodSums))
+plot(days, cumsum(badSums))
+plot(days, cumsum(goodSums - badSums))
 legend('Hyvinvointi', 'Pahoinvointi', 'Pisteiden erotus')
 ylabel('Pisteet')
-
-
-
-    
